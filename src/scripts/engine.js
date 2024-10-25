99da70c21f5d98cfa63e62c9d89d76e455e6545a
duarte-arthur21 <ferreiraduarte21@gmail.com>
2024-10-25 00:24:26 -0300

    Implementando o Listener de Hitbox

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,24 +1,27 @@
 const state = {
     view : {
-        squares: document.querySelectorAll("square"),
-        enemy : document.querySelectorAll(".enemy"),
-        timeleft: document.querySelector("#time-left"),
+        squares: document.querySelectorAll(".square"),
+        enemy : document.querySelector(".enemy"),
+        timeLeft: document.querySelector("#time-left"),
         score: document.querySelector("#score"),
     },
     values: {
         timerId: null,
         gameVelocity: 1000,
+        hitPosition: 0,
+        result: 0,
     },
 };
 function randomSquare(){
-    state.view.squares.forEach((square)=>{
+    state.view.squares.forEach((square) => {
         square.classList.remove("enemy");
     });
 
-    let randomNumber = math.floor(Math.random() * 9);
-    let randmSquare = state.view.squares[randomNumber];
+    let randomNumber = Math.floor(Math.random() * 9);
+    let randomSquare = state.view.squares[randomNumber];
     randomSquare.classList.add("enemy");
-}
+    state.values.hitPosition = randomSquare.id;
+};
 
 function moveEnemy(){
     state.values.timerId = setInterval(randomSquare, state.values.gameVelocity);
@@ -26,7 +29,13 @@ function moveEnemy(){
 
 function addListenerHitbox(){
     state.view.squares.forEach((square) => {
-        // if(square.id === )
+        square.addEventListener("mousedown", ()=> {
+            if(square.id === state.values.hitPosition) {
+                state.values.result++;
+                state.view.score.textContent = state.values.result;
+                state.values.hitPosition = null;
+            }
+        });
     });
 }
 
